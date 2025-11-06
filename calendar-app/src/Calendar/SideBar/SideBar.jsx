c2806bb881810c1d42f4cb928646620a4f98deaf
SimonX1ao <simonxiao81@gmail.com>
2025-11-06 15:39:07 -0500

    added the mini calendar

--- calendar-app/src/Calendar/SideBar/SideBar.jsx
+++ calendar-app/src/Calendar/SideBar/SideBar.jsx
@@ -4,8 +4,84 @@ function SideBar(){
     return(
         <>
             <div className="side-bar">
-                <div className="side-bar-calendar-grids">calendar grids</div>
-                <div className="side-bar-progress">tasks</div>
+                <div className="calendar">
+                    <div className="calendar-header">
+                        <div className="calendar-date">{`November 2025`}</div>
+                        <div className="calendar-header-btns">
+                            <button className="left-btn btn"><i className='bx bx-chevron-left'></i></button>
+                            <button className="right-btn btn"><i className='bx bx-chevron-right'></i></button>
+                        </div>
+                    </div>
+                    <div className="calendar-grids">    
+                        <table className="days-of-week">
+                            <thead>
+                                <tr>
+                                    <th>S</th>
+                                    <th>M</th>
+                                    <th>T</th>
+                                    <th>W</th>
+                                    <th>T</th>
+                                    <th>F</th>
+                                    <th>S</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>1</td>
+                                    <td>2</td>
+                                    <td>3</td>
+                                    <td>4</td>
+                                    <td>5</td>
+                                    <td>6</td>
+                                    <td>7</td>
+                                </tr>
+                                <tr>
+                                    <td>8</td>
+                                    <td>9</td>
+                                    <td>10</td>
+                                    <td>11</td>
+                                    <td>12</td>
+                                    <td>13</td>
+                                    <td>14</td>
+                                </tr>
+                                <tr>
+                                    <td>15</td>
+                                    <td>16</td>
+                                    <td>17</td>
+                                    <td>18</td>
+                                    <td>19</td>
+                                    <td>20</td>
+                                    <td>21</td>
+                                </tr>
+                                <tr>
+                                    <td>22</td>
+                                    <td>23</td>
+                                    <td>24</td>
+                                    <td>25</td>
+                                    <td>26</td>
+                                    <td>27</td>
+                                    <td>28</td>
+                                </tr>
+                                <tr>
+                                    <td>29</td>
+                                    <td>30</td>
+                                    <td>31</td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                    <td></td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                </div>
+                <div className="progress">
+                    <div className="progress-title">
+                        <span>Tasks</span>
+                        <button><i class='bx bx-plus'></i></button>
+                    </div>
+
+                </div>
             </div>
         </>
     );
