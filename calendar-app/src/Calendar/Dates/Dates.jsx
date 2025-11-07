6b36468f34bc929522618531a241af1caac37a26
SimonX1ao <simonxiao81@gmail.com>
2025-11-06 23:03:59 -0500

    added the header of the main calendar

--- calendar-app/src/Calendar/Dates/Dates.jsx
+++ calendar-app/src/Calendar/Dates/Dates.jsx
@@ -2,49 +2,35 @@ import "./Dates.css"
 function Dates(){
     return(
         <div className="dates">
-            <div className="dates-header">
+            <div className="header">
                 <button>Today</button>
                 <i class='bx  bx-chevron-left'></i> 
                 <i class='bx  bx-chevron-right'></i> 
                 <span>November 2025</span>
             </div>
-            <div className="Dates-calendar-grids">
-                <table className="Dates-calendar-table">
-                    <thead>
-                        <th></th>
-                        <th>
-                            <span>SUN</span>
-                            <div>2</div>
-                        </th>
-                        <th>
-                            <span>MON</span>
-                            <div>3</div>
-                        </th>
-                        <th>
-                            <span>TUE</span>
-                            <div>4</div>
-                        </th>
-                        <th>
-                            <span>WED</span>
-                            <div>5</div>
-                        </th>
-                        <th>
-                            <span>THU</span>
-                            <div>6</div>
-                        </th>
-                        <th>
-                            <span>FRI</span>
-                            <div>7</div>
-                        </th>
-                        <th>
-                            <span>SAT</span>
-                            <div>8</div>
-                        </th>
-                    </thead>
-                    <tr>
-                        <td></td>
-                    </tr>
-                </table>
+            <div className="calendar">
+                <div className="header">
+                    <div>
+                        <span>GMT-05</span>
+                    </div>
+                    <div className="day">SUN</div>
+                    <div className="day">MON</div>
+                    <div className="day">TUE</div>
+                    <div className="day">WED</div>
+                    <div className="day">THU</div>
+                    <div className="day">FRI</div>
+                    <div className="day">SAT</div>
+                </div> 
+                <div className="body">
+                    <div className="column"></div>
+                    <div className="column"></div>
+                    <div className="column"></div>
+                    <div className="column"></div>
+                    <div className="column"></div>
+                    <div className="column"></div>
+                    <div className="column"></div>
+                    <div className="column"></div>
+                </div>
             </div>
         </div>
     )
