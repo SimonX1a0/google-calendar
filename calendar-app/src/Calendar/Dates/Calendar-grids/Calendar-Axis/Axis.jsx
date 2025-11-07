5b8066517596d5e19a7bc5a27fd0c31619136963
SimonX1ao <simonxiao81@gmail.com>
2025-11-07 12:23:02 -0500

    reduced the axis code using a for loop and a map function

--- calendar-app/src/Calendar/Dates/Calendar-grids/Calendar-Axis/Axis.jsx
+++ calendar-app/src/Calendar/Dates/Calendar-grids/Calendar-Axis/Axis.jsx
@@ -1,32 +1,18 @@
 import style from "./Axis.module.css"
 
 function Axis(){
+    const times = [];
+    for(let i=1; i< 24; i++){
+        let suffix = (i<12) ? 'am' : 'pm'
+        let display = i%12 + suffix;
+        times.push(display);
+    }
     return(
         <div className={style.axis}>
             <div></div>
-            <div><span>1am</span></div>
-            <div><span>2am</span></div>
-            <div><span>3am</span></div>
-            <div><span>4am</span></div>
-            <div><span>5am</span></div>
-            <div><span>6am</span></div>
-            <div><span>7am</span></div>
-            <div><span>8am</span></div>
-            <div><span>9am</span></div>
-            <div><span>10am</span></div>
-            <div><span>11am</span></div>
-            <div><span>12pm</span></div>
-            <div><span>1pm</span></div>
-            <div><span>2pm</span></div>
-            <div><span>3pm</span></div>
-            <div><span>4pm</span></div>
-            <div><span>5pm</span></div>
-            <div><span>6pm</span></div>
-            <div><span>7pm</span></div>
-            <div><span>8pm</span></div>
-            <div><span>9pm</span></div>
-            <div><span>10pm</span></div>
-            <div><span>11pm</span></div>
+            {times.map((time)=>(
+                <div><span>{time}</span></div>
+            ))}
         </div>
     );
 }
