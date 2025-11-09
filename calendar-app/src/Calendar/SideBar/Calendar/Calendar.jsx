0a1564597cf4dfacbf76fa4ededda22bc6c85f2d
SimonX1ao <simonxiao81@gmail.com>
2025-11-08 22:27:30 -0500

    display real dates in the mini calendar

--- calendar-app/src/Calendar/SideBar/Calendar/Calendar.jsx
+++ calendar-app/src/Calendar/SideBar/Calendar/Calendar.jsx
@@ -1,6 +1,41 @@
 import style from "./Calendar.module.css"
 function Calendar(){
-    let weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S']
+    let weekDays = ['S', 'M', 'T', 'W', 'T', 'F', 'S'];
+    const now = new Date();
+    const year = now.getFullYear();
+    const month = now.getMonth();
+
+    const firstDay = new Date(year, month, 1).getDay();
+    const daysInMonth = new Date(year, month+1 , 0).getDate();
+    const lastDay = new Date(year, month, daysInMonth).getDay();
+    const preMonthDays = new Date(year, month, 0).getDate();
+
+    const dates = [];
+
+    for(let i=firstDay-1; i>=0; i--){
+        dates.push({
+            day: preMonthDays - i,
+            isCurrMon: false,
+            isCurrDay:false,
+        });
+    }
+
+    for(let i=1; i<=daysInMonth; i++){
+        dates.push({
+            day: i,
+            isCurrMon: true,
+            isCurrDay: (i == now.getDate()) ? true : false,
+        });
+    }
+
+    for(let i=lastDay+1; i < 7; i++){
+        dates.push({
+            day: i,
+            isCurrMon: false,
+            isCurrDay: false,
+        });
+    }
+
     return(
         <div className={style.calendar}>
             <div className={style.header}>
@@ -15,8 +50,8 @@ function Calendar(){
                     <div className={style.daysTitle} key={i}>{day}</div>
                 ))}
                   
-                {Array.from({length: 35}).map((_, i)=>(
-                    <div key={i} className={style.day}>{i+1}</div>
+                {dates.map((date, i)=>(
+                    <div key={i} className={`${style.day} ${(date.isCurrDay) ? style.today : ""}`}>{date.day}</div>
                 ))}
             </div>
         </div>
