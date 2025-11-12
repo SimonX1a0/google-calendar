b610136f2c8a457d82dc235fbc04dbb197de3b11
SimonX1ao <simonxiao81@gmail.com>
2025-11-12 15:16:35 -0500

    fixed the main calendar heading logic

--- calendar-app/src/Calendar/Dates/Dates-Header/DatesHeader.jsx
+++ calendar-app/src/Calendar/Dates/Dates-Header/DatesHeader.jsx
@@ -1,12 +1,47 @@
 import style from "./DatesHeader.module.css"
-
+import { useContext } from "react";
+import { CalendarContext } from "../../Calendar.jsx";
 function DatesHeader(){
+
+    const ctx = useContext(CalendarContext);
+    const previewDate = ctx.previewDate;
+    const setPreviewDate = ctx.setPreviewDate;
+    const setMiniDate = ctx.setMiniDate;
+
+    const today = ()=>{
+        const now = new Date();
+        setPreviewDate(now);
+        setMiniDate(now);
+    }
+
+    const nextWeek=()=>{
+        setPreviewDate(new Date(previewDate.getFullYear(), previewDate.getMonth(), previewDate.getDate()+7));
+        setMiniDate(new Date(previewDate.getFullYear(), previewDate.getMonth(), previewDate.getDate()+7));
+        console.log(previewDate);
+    }
+
+    const lastWeek=()=>{
+        setPreviewDate(new Date(previewDate.getFullYear(), previewDate.getMonth(), previewDate.getDate()-7));
+        setMiniDate(new Date(previewDate.getFullYear(), previewDate.getMonth(), previewDate.getDate()-7));
+        console.log(previewDate);
+    }
+
+
     return(
         <>
             <div className={style.header}>
-                <button className={style.today}>Today</button>
-                <i className={`bx  bx-chevron-left ${style.left}`}></i> 
-                <i className={`bx  bx-chevron-right ${style.right}`}></i> 
+                <button
+                    className={style.today}
+                    onClick={today}
+                >Today</button>
+                <i
+                    className={`bx  bx-chevron-left ${style.left}`}
+                    onClick={lastWeek}
+                ></i> 
+                <i 
+                    className={`bx  bx-chevron-right ${style.right}`}
+                    onClick={nextWeek}
+                ></i> 
                 <span className={style.date}>{`November 2025`}</span>
             </div>
         </>
