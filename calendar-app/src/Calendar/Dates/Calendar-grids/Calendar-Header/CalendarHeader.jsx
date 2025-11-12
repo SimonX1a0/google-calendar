35b85df4c19cafd056e527ca6fae951820656d03
SimonX1ao <simonxiao81@gmail.com>
2025-11-12 15:32:39 -0500

    dynamically generate dates in the main calendar

--- calendar-app/src/Calendar/Dates/Calendar-grids/Calendar-Header/CalendarHeader.jsx
+++ calendar-app/src/Calendar/Dates/Calendar-grids/Calendar-Header/CalendarHeader.jsx
@@ -1,14 +1,23 @@
 import style from "./CalendarHeader.module.css"
 import HeaderDay from "./HeaderDay.jsx"
-
+import { useContext } from "react";
+import { CalendarContext } from "../../../Calendar.jsx";
 function CalendarHeader() {
-
+  const ctx = useContext(CalendarContext);
+  const previewDate = ctx.previewDate;
+  const day = previewDate.getDay();
+  const startDate = new Date(previewDate.getFullYear(), previewDate.getMonth(), previewDate.getDate() - day);
+  
   const days = [];
   for (let i = 0; i < 7; i++) {
     const weekday = new Date(2025, 11, i).toLocaleString("default", { weekday: "short" }).toUpperCase();
-    const dayNum = i;
+    const dayNum = startDate.getDate() + i;
+    const isToday = previewDate.getFullYear() === new Date().getFullYear() &&
+                    previewDate.getMonth() === new Date().getMonth() &&
+                    dayNum === new Date().getDate();
 
-    days.push({ weekday, dayNum });
+
+    days.push({ weekday, dayNum, isToday });
   }
 
   return (
--- calendar-app/src/Calendar/Dates/Dates.jsx
+++ calendar-app/src/Calendar/Dates/Dates.jsx
@@ -1,18 +1,11 @@
 import style from "./Dates.module.css"
 import DatesHeader from "./Dates-Header/DatesHeader.jsx"
 import CalendarGrid from "./Calendar-grids/CalendarGrids.jsx"
-function Dates(prop){
-    const currentDate = prop.currentDate;
-    const lastWeek = prop.lastWeek;
-    const nextWeek = prop.nextWeek;
+function Dates(){
     return(
         <div className={style.dates}>
-            <DatesHeader
-            currentDate={currentDate}
-            lastWeek={lastWeek}
-            nextWeek={nextWeek}></DatesHeader>
-            <CalendarGrid
-            currentDate={currentDate}></CalendarGrid>
+            <DatesHeader></DatesHeader>
+            <CalendarGrid></CalendarGrid>
         </div>
     )
 }
