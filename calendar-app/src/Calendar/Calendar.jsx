9ba76b255879d8dc6aa55025c15009b4fc799e8f
SimonX1ao <simonxiao81@gmail.com>
2025-11-13 21:08:20 -0500

    added tasklist

--- calendar-app/src/Calendar/Calendar.jsx
+++ calendar-app/src/Calendar/Calendar.jsx
@@ -6,11 +6,18 @@ export const CalendarContext=createContext();
 function Calendar(){
     const [previewDate,setPreviewDate]=useState(new Date());
     const [miniDate,setMiniDate]=useState(new Date());
+    const [taskList,setTaskList]=useState([{
+        id:1,
+        title:"Sample Task",
+        category:"Work",
+        startTime:new Date(),
+        totalTime:60,
+    }]);
     
     return(
         <>
             <div className="calendar-app">
-                <CalendarContext.Provider value={{previewDate,setPreviewDate,miniDate,setMiniDate}}>
+                <CalendarContext.Provider value={{previewDate,setPreviewDate,miniDate,setMiniDate,taskList,setTaskList}}>
                     <SideBar/>
                     <Dates/>
                 </CalendarContext.Provider>
