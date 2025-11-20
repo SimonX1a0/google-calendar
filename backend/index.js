3739c436fd7980fe024d9934b8d8590a6c2a7820
SimonX1ao <simonxiao81@gmail.com>
2025-11-20 15:30:57 -0500

    set up the server

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,14 @@
 import express from "express";
-import cors from 'cors'
+import cors from "cors"
+import dotenv from "dotenv"
+import connectDB from "./config/db.js"
 
+dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+await connectDB();
 
 app.get("/", (req, res)=>{
     res.send("backend is running");
@@ -11,4 +16,5 @@ app.get("/", (req, res)=>{
 
 app.listen(4000, ()=>{
     console.log("server running on localhost:4000");
+    console.log(process.pid);
 });
